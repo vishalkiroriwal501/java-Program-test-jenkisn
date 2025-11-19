d4fa0830ce9c65fd8d52e9d4ed0db47ef46e54ca
Vishal Kumar <vishal@aucioinfotech.com>
2025-11-19 14:22:48 +0530

    Update Main.java to add a new message for Jenkins testing

--- Main.java
+++ Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        System.out.println("this is the new program to chekck that ke jenkins works proper or not! ");
+        
     }
 }
